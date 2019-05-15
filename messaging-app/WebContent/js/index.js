14742f946e846f5e4668cccb07f8c683221bd909
MounikaMylaram <mounikaa.mylaram@valuelabs.net>
2019-05-15 21:34:37 +0530

    html files

---
+++ messaging-app/WebContent/js/index.js
@@ -0,0 +1,64 @@
+function login(){
+	//var XMLHTTPRequest = new XMLHTTPRequest();
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+ if(username == null || username.length ==0){
+     return;
+ }else if(password == null || password.length == 0){
+     return;
+ }
+ var requestData = {"email" : username,"password" : password};
+ var data = JSON.stringify(requestData);
+ $.ajax({
+     type: "POST",
+     url: "/messaging-app/login",
+     contentType: "application/json",
+     data:data,
+     async: false, //add this
+   }).done(function ( data ) {
+         Success = true;
+         if(data.data == undefined || data.statusMessage == "USER_NOT_AVAILABLE"){
+        	alert("user not available");
+         }else{
+        	 alert("Success"+ JSON.stringify(data));
+             
+             var userData = data.data;
+             alert("userData"+userData.extId);
+             
+             setCookie("extid",userData.extid,1);
+             setCookie("name",userData.name,1);
+             setCookie("email",userData.email,1);
+             window.location.href = './postmessage.html';
+             alert("naviagate");
+         }
+         
+ }).fail(function ( data ) {
+        Success = false;
+        alert("data");
+ });
+
+}
+	
+function setCookie(cname,cvalue,exdays) {
+	  var d = new Date();
+	  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	  var expires = "expires=" + d.toGMTString();
+	  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+	}
+
+function getCookie(cname) {
+	  var name = cname + "=";
+	  var decodedCookie = decodeURIComponent(document.cookie);
+	  var ca = decodedCookie.split(';');
+	  for(var i = 0; i < ca.length; i++) {
+	    var c = ca[i];
+	    while (c.charAt(0) == ' ') {
+	      c = c.substring(1);
+	    }
+	    if (c.indexOf(name) == 0) {
+	      return c.substring(name.length, c.length);
+	    }
+	  }
+	  return "";
+	}
